86940bba00de9ff71c70d5a4180e48879021b49a
trichimtrich <trichimtrich@gmail.com>
2019-05-25 00:19:32 +0800

    WIP share

--- static/js/init.js
+++ static/js/init.js
@@ -14,10 +14,31 @@ $(window).on("resize", fixScreenLayout);
 $(window).on("orientationchange", fixScreenLayout);
 
 
+function parseURLForRoom() {
+    var queryDict = {}
+    location.search.substr(1).split("&").forEach(function(item) {
+        queryDict[item.split("=")[0]] = item.split("=")[1]
+    });
+    if (typeof queryDict["id"] === "string") {
+        return queryDict["id"];
+    }
+    return null;
+}
+
 $(document).ready(function () {
     fixScreenLayout();
 
+
+    // localStorage first
     roomID = loadRoomID();
+
+    // Shared URL second
+    var rid = parseURLForRoom();
+    if (rid !== null) {
+        roomID = rid;
+    }
+    // if from URL -> start game immediately!
+
     $("#room-txt").val(roomID);
 });
 
