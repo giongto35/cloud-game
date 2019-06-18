102ce66fe07136ca832e1321dd8ffade2a324eea
giongto35 <giongto35@yahoo.com>
2019-06-19 04:23:33 +0800

    Update latency

--- cws/cws.go
+++ cws/cws.go
@@ -2,7 +2,6 @@ package cws
 
 import (
 	"encoding/json"
-	"fmt"
 	"log"
 	"sync"
 	"time"
@@ -158,10 +157,6 @@ func (c *Client) Listen() {
 		}
 		wspacket := WSPacket{}
 		err = json.Unmarshal(rawMsg, &wspacket)
-		fmt.Println(wspacket)
-		if wspacket.ID == "checkLatency" {
-			fmt.Println("!!!!!!")
-		}
 
 		if err != nil {
 			continue
