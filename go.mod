85873092fcafc8bafdaa562aca626a1201e83c45
Sergy Stepanov <sergystepanov@gmail.com>
2019-10-14 17:07:10 +0300

    Use Go x/image lib for interpolation

--- go.mod
+++ go.mod
@@ -11,5 +11,6 @@ require (
 	github.com/pion/webrtc/v2 v2.1.2
 	github.com/prometheus/client_golang v1.1.0
 	github.com/spf13/pflag v1.0.3
+	golang.org/x/image v0.0.0-20191009234506-e7c1f5e7dbb8
 	gopkg.in/hraban/opus.v2 v2.0.0-20180426093920-0f2e0b4fc6cd
 )
--- pkg/emulator/libretro/image/scale.go
+++ pkg/emulator/libretro/image/scale.go
@@ -1,7 +1,7 @@
 package image
 
-import "C"
 import (
+	"golang.org/x/image/draw"
 	"image"
 	"image/color"
 )
@@ -17,24 +17,31 @@ const (
 	ScaleBilinear = 2
 )
 
-func Resize(scaleType int, fn Format, w int, h int, packedW int, vw int, vh int, bpp int, data []byte, image *image.RGBA) {
+func Resize(scaleType int, fn Format, w int, h int, packedW int, vw int, vh int, bpp int, data []byte, out *image.RGBA) {
+
+	// !to implement own image interfaces img.Pix = bytes[]
+	src := image.NewRGBA(image.Rect(0, 0, w, h))
+	toRgba(fn, w, h, packedW, bpp, data, src)
+
 	// !to do set it once instead switching on each iteration
 	// !to do skip resize if w=vw h=vh
 	switch scaleType {
 	case ScaleSkip:
-		skip(fn, w, h, packedW, vw, vh, bpp, data, image)
+		skip(fn, w, h, packedW, vw, vh, bpp, data, src, out)
 	case ScaleNearestNeighbour:
-		nearest(fn, w, h, packedW, vw, vh, bpp, data, image)
+		draw.NearestNeighbor.Scale(out, out.Bounds(), src, src.Bounds(), draw.Src, nil)
+		//nearest(fn, w, h, packedW, vw, vh, bpp, data, src, out)
 	case ScaleBilinear:
-		bilinear(fn, w, h, packedW, vw, vh, bpp, data, image)
+		draw.ApproxBiLinear.Scale(out, out.Bounds(), src, src.Bounds(), draw.Src, nil)
+		//bilinear(fn, w, h, packedW, vw, vh, bpp, data, src, out)
 	case ScaleOld:
 		fallthrough
 	default:
-		old(fn, w, h, packedW, vw, vh, bpp, data, image)
+		old(fn, w, h, packedW, vw, vh, bpp, data, src, out)
 	}
 }
 
-func old(fn Format, w int, h int, packedW int, vw int, vh int, bpp int, data []byte, image *image.RGBA) {
+func old(fn Format, w int, h int, packedW int, vw int, vh int, bpp int, data []byte, _ *image.RGBA, out *image.RGBA) {
 	seek := 0
 
 	scaleWidth := float64(vw) / float64(w)
@@ -45,7 +52,7 @@ func old(fn Format, w int, h int, packedW int, vw int, vh int, bpp int, data []b
 		for x := 0; x < packedW; x++ {
 			x2 := int(float64(x) * scaleWidth)
 			if x2 < vw {
-				image.Set(x2, y2, fn(data, seek))
+				out.Set(x2, y2, fn(data, seek))
 			}
 
 			seek += bpp
@@ -53,17 +60,17 @@ func old(fn Format, w int, h int, packedW int, vw int, vh int, bpp int, data []b
 	}
 }
 
-func skip(fn Format, w int, h int, packedW int, _ int, _ int, bpp int, data []byte, image *image.RGBA) {
+func skip(fn Format, w int, h int, packedW int, _ int, _ int, bpp int, data []byte, _ *image.RGBA, out *image.RGBA) {
 	for y := 0; y < h; y++ {
 		for x := 0; x < w; x++ {
 			index := (y * packedW) + x
 			index *= bpp
-			image.Set(x, y, fn(data, index))
+			out.Set(x, y, fn(data, index))
 		}
 	}
 }
 
-func nearest(fn Format, w int, h int, packedW int, vw int, vh int, bpp int, data []byte, image *image.RGBA) {
+func nearest(fn Format, w int, h int, packedW int, vw int, vh int, bpp int, data []byte, _ *image.RGBA, out *image.RGBA) {
 	xRatio := ((w << 16) / vw) + 1
 	yRatio := ((h << 16) / vh) + 1
 
@@ -75,12 +82,13 @@ func nearest(fn Format, w int, h int, packedW int, vw int, vh int, bpp int, data
 			index := (y2 * packedW) + x2
 			index *= bpp
 
-			image.Set(x, y, fn(data, index))
+			out.Set(x, y, fn(data, index))
 		}
 	}
 }
 
-func bilinear(fn Format, w int, h int, packedW int, vw int, vh int, bpp int, data []byte, image *image.RGBA) {
+// This implementation has some color bleeding issues
+func bilinear(fn Format, w int, h int, packedW int, vw int, vh int, bpp int, data []byte, _ *image.RGBA, out *image.RGBA) {
 	xRatio := float64(w-1) / float64(vw)
 	yRatio := float64(h-1) / float64(vh)
 
@@ -99,13 +107,28 @@ func bilinear(fn Format, w int, h int, packedW int, vw int, vh int, bpp int, dat
 			c := fn(data, (index+packedW)*bpp)
 			d := fn(data, (index+packedW+1)*bpp)
 
-			image.Set(x, y, color.RGBA{
+			out.Set(x, y, color.RGBA{
 				// don't sink the boat
 				R: byte(float64(a.R)*(1-w)*(1-h) + float64(b.R)*w*(1-h) + float64(c.R)*h*(1-w) + float64(d.R)*w*h),
 				G: byte(float64(a.G)*(1-w)*(1-h) + float64(b.G)*w*(1-h) + float64(c.G)*h*(1-w) + float64(d.G)*w*h),
 				B: byte(float64(a.B)*(1-w)*(1-h) + float64(b.B)*w*(1-h) + float64(c.B)*h*(1-w) + float64(d.B)*w*h),
-				A: byte(float64(a.A)*(1-w)*(1-h) + float64(b.A)*w*(1-h) + float64(c.A)*h*(1-w) + float64(d.A)*w*h),
+				//A: byte(float64(a.A)*(1-w)*(1-h) + float64(b.A)*w*(1-h) + float64(c.A)*h*(1-w) + float64(d.A)*w*h),
 			})
+		}
+	}
+}
+
+func toRgba(fn Format, w int, h int, packedW int, bpp int, data []byte, image *image.RGBA) {
+	for y := 0; y < h; y++ {
+		for x := 0; x < w; x++ {
+			index := (y*packedW + x) * bpp
+			c := fn(data, index)
+			i := (y-image.Rect.Min.Y)*image.Stride + (x-image.Rect.Min.X)*4
+			s := image.Pix[i : i+4 : i+4]
+			s[0] = c.R
+			s[1] = c.G
+			s[2] = c.B
+			s[3] = c.A
 		}
 	}
 }
