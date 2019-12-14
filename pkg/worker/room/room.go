7ad65f657931d6ae3c1d9184cc28231ff93de17c
giongto35 <giongto35@yahoo.com>
2019-12-14 12:26:23 +0800

    Clean up

--- pkg/worker/room/room.go
+++ pkg/worker/room/room.go
@@ -216,7 +216,6 @@ func (r *Room) startWebRTCSession(peerconnection *webrtc.WebRTC) {
 			// the first 10 bits belong to player 1
 			// the next 10 belongs to player 2 ...
 			// We standardize and put it to inputChannel (20 bits)
-			//input = input << ((uint(playerIndex) - 1) * config.NumKeys)
 			select {
 			case r.inputChannel <- nanoarch.InputEvent{KeyState: input, PlayerIdx: peerconnection.GameMeta.PlayerIndex}:
 			default:
