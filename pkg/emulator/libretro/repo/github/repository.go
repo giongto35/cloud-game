90b719e549943492374a4711739f990b6220ce47
Sergey Stepanov <sergystepanov@gmail.com>
2020-12-28 19:13:51 +0300

    Add GitHub repo backend for the core downloader

--- pkg/emulator/libretro/repo/github/repository.go
+++ pkg/emulator/libretro/repo/github/repository.go
@@ -3,15 +3,18 @@ package github
 import (
 	"github.com/giongto35/cloud-game/v2/pkg/emulator/libretro/core"
 	"github.com/giongto35/cloud-game/v2/pkg/emulator/libretro/repo"
+	"github.com/giongto35/cloud-game/v2/pkg/emulator/libretro/repo/buildbot"
 )
 
 type Repo struct {
+	buildbot.Repo
 }
 
-func NewGithubRepo() Repo {
-	return Repo{}
+func NewGithubRepo(address string, compression string) Repo {
+	return Repo{Repo: buildbot.NewBuildbotRepo(address, compression)}
 }
 
 func (r Repo) GetCoreData(file string, info core.ArchInfo) repo.Data {
-	return repo.Data{}
+	dat := r.Repo.GetCoreData(file, info)
+	return repo.Data{Url: dat.Url + "?raw=true", Compression: dat.Compression}
 }
