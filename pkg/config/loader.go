d2609000122e60af62b37596fdba498ebc0ed794
Sergey Stepanov <sergystepanov@gmail.com>
2020-12-30 20:41:46 +0300

    Add env variables reader with CLOUD_GAME prefix

--- pkg/config/loader.go
+++ pkg/config/loader.go
@@ -6,20 +6,19 @@ import (
 	"github.com/kkyr/fig"
 )
 
-type Loader interface {
-	LoadConfig(config interface{}, path string) interface{}
-}
-
 // LoadConfig loads a configuration file into the given struct.
 // The path param specifies a custom path to the configuration file.
+// Reads and puts environment variables with the prefix CLOUD_GAME_.
+// Params from the config should be in uppercase separated with _.
 func LoadConfig(config interface{}, path string) interface{} {
+	envPrefix := "CLOUD_GAME"
 	dirs := []string{path}
 	if path == "" {
 		if home, err := os.UserHomeDir(); err == nil {
 			dirs = append(dirs, ".", "configs", home+"/.cr", "../../../configs")
 		}
 	}
-	if err := fig.Load(config, fig.Dirs(dirs...)); err != nil {
+	if err := fig.Load(config, fig.Dirs(dirs...), fig.UseEnv(envPrefix)); err != nil {
 		panic(err)
 	}
 	return config
