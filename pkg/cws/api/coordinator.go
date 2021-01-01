c9c62a9f2cc61fddce247f2912125370829baf1b
Sergey Stepanov <sergystepanov@gmail.com>
2021-01-01 20:38:16 +0300

    Add internal coordinator API

---
+++ pkg/cws/api/coordinator.go
@@ -0,0 +1,27 @@
+package api
+
+import "github.com/giongto35/cloud-game/v2/pkg/cws"
+
+const (
+	ConfigRequest = "config_request"
+)
+
+type GameStartRequest struct {
+	GameName string `json:"game_name"`
+	IsMobile bool   `json:"is_mobile"`
+}
+
+func (packet *GameStartRequest) From(data string) error { return from(packet, data) }
+
+type GameStartCall struct {
+	Name string `json:"name"`
+	Path string `json:"path"`
+	Type string `json:"type"`
+}
+
+func (packet *GameStartCall) From(data string) error { return from(packet, data) }
+func (packet *GameStartCall) To() (string, error)    { return to(packet) }
+
+// packets
+
+func ConfigPacket() cws.WSPacket { return cws.WSPacket{ID: ConfigRequest} }
