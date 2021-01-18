1f4c225a8003222fe32abe1b59950a3f195e6a83
sergystepanov <SergyStepanov@gmail.com>
2021-01-18 23:20:50 +0300

    Refactor OPUS encoder for quality and speed (#262)
    * Refactor OPUS encoder for quality and speed.
    
    Use OPUS low delay mode (https://www.opus-codec.org/docs/opus_api-1.1.3/group__opus__ctlvalues.html#ga592232fb39db60c1369989c5c5d19a07)
    
    * Slightly tweak linear resample function and disable it on 48kHz input
    
    * Rewrite OPUS encoder as a struct with some internal buffer
    
    * Use OPUS 192Kbps preset by default
    
    * Add encoder callback function on a full buffer
    
    * Reuse OPUS output buffer
    
    * Add write limiter for samples

--- pkg/worker/room/media.go
+++ pkg/worker/room/media.go
@@ -7,37 +7,10 @@ import (
 	encoderConfig "github.com/giongto35/cloud-game/v2/pkg/config/encoder"
 	"github.com/giongto35/cloud-game/v2/pkg/encoder"
 	"github.com/giongto35/cloud-game/v2/pkg/encoder/h264encoder"
+	"github.com/giongto35/cloud-game/v2/pkg/encoder/opus"
 	vpxencoder "github.com/giongto35/cloud-game/v2/pkg/encoder/vpx-encoder"
-	"github.com/giongto35/cloud-game/v2/pkg/util"
 	"github.com/giongto35/cloud-game/v2/pkg/webrtc"
-	"gopkg.in/hraban/opus.v2"
 )
-
-func resample(pcm []int16, targetSize int, srcSampleRate int, dstSampleRate int) []int16 {
-	newPCML := make([]int16, targetSize/2)
-	newPCMR := make([]int16, targetSize/2)
-	newPCM := make([]int16, targetSize)
-	for i := 0; i+1 < len(pcm); i += 2 {
-		newPCML[(i/2)*dstSampleRate/srcSampleRate] = pcm[i]
-		newPCMR[(i/2)*dstSampleRate/srcSampleRate] = pcm[i+1]
-	}
-	for i := 1; i < len(newPCML); i++ {
-		if newPCML[i] == 0 {
-			newPCML[i] = newPCML[i-1]
-		}
-	}
-	for i := 1; i < len(newPCMR); i++ {
-		if newPCMR[i] == 0 {
-			newPCMR[i] = newPCMR[i-1]
-		}
-	}
-	for i := 0; i+1 < targetSize; i += 2 {
-		newPCM[i] = newPCML[i/2]
-		newPCM[i+1] = newPCMR[i/2]
-	}
-
-	return newPCM
-}
 
 func (r *Room) startVoice() {
 	// broadcast voice
@@ -64,59 +37,34 @@ func (r *Room) startVoice() {
 }
 
 func (r *Room) startAudio(sampleRate int, audio encoderConfig.Audio) {
-	log.Println("Enter fan audio")
-
-	srcSampleRate := sampleRate
-	enc, err := opus.NewEncoder(audio.Frequency, audio.Channels, opus.AppAudio)
+	sound, err := opus.NewEncoder(
+		sampleRate,
+		audio.Frequency,
+		audio.Channels,
+		opus.SampleBuffer(audio.Frame, sampleRate != audio.Frequency),
+		// we use callback on full buffer in order to
+		// send data to all the clients ASAP
+		opus.CallbackOnFullBuffer(r.broadcastAudio),
+	)
 	if err != nil {
-		log.Println("[!] Cannot create audio encoder", err)
+		log.Fatalf("error: cannot create audio encoder, %v", err)
+	}
+	log.Printf("OPUS: %v", sound.GetInfo())
+
+	for samples := range r.audioChannel {
+		sound.BufferWrite(samples)
 	}
 
-	enc.SetMaxBandwidth(opus.Fullband)
-	enc.SetBitrateToAuto()
-	enc.SetComplexity(10)
-
-	dstBufferSize := audio.GetFrameDuration()
-	srcBufferSize := dstBufferSize * srcSampleRate / audio.Frequency
-	pcm := make([]int16, srcBufferSize) // 640 * 1000 / 16000 == 40 ms
-	idx := 0
-
-	// fanout Audio
-	for sample := range r.audioChannel {
-		for i := 0; i < len(sample); {
-			rem := util.MinInt(len(sample)-i, len(pcm)-idx)
-			copy(pcm[idx:idx+rem], sample[i:i+rem])
-			i += rem
-			idx += rem
-
-			if idx == len(pcm) {
-				data := make([]byte, 1024*2)
-				dstpcm := resample(pcm, dstBufferSize, srcSampleRate, audio.Frequency)
-				n, err := enc.Encode(dstpcm, data)
-
-				if err != nil {
-					log.Println("[!] Failed to decode", err)
-
-					idx = 0
-					continue
-				}
-				data = data[:n]
-
-				// TODO: r.rtcSessions is rarely updated. Lock will hold down perf
-				//r.sessionsLock.Lock()
-				for _, webRTC := range r.rtcSessions {
-					if webRTC.IsConnected() {
-						// NOTE: can block here
-						webRTC.AudioChannel <- data
-					}
-				}
-
-				idx = 0
-			}
-		}
-
-	}
 	log.Println("Room ", r.ID, " audio channel closed")
+}
+
+func (r *Room) broadcastAudio(audio []byte) {
+	for _, webRTC := range r.rtcSessions {
+		if webRTC.IsConnected() {
+			// NOTE: can block here
+			webRTC.AudioChannel <- audio
+		}
+	}
 }
 
 // startVideo processes imageChannel images with an encoder (codec) then pushes the result to WebRTC.
