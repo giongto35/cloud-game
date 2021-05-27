85e3b22d5e566cf488356b69acbff7f0fbd6de1b
giongto35 <giongto35@yahoo.com>
2021-05-28 01:25:19 +0800

    Redeploy specific

--- .github/workflows/redeploy/redeploy_specific.sh
+++ .github/workflows/redeploy/redeploy_specific.sh
@@ -25,8 +25,9 @@ do
     iptables -t nat -A PREROUTING -p tcp --dport 80 -j REDIRECT --to-port $httpport; iptables-save;
     echo $PASSWORD | docker login https://docker.pkg.github.com --username $USERNAME --password-stdin; 
     docker system prune -f;
-    source /etc/profile; docker rm cloud-game -f;
+    source /etc/profile; 
     docker pull docker.pkg.github.com/giongto35/cloud-game/cloud-game:latest;
+    docker rm cloud-game -f;
     docker run --privileged -d --network=host --env DISPLAY=:99 --env MESA_GL_VERSION_OVERRIDE=3.3 -v cores:/usr/local/share/cloud-game/assets/cores -v /cloud-game/configs:/usr/local/share/cloud-game/configs -v /cloud-game/games:/usr/local/share/cloud-game/assets/games -v /cloud-game/cache:/usr/local/share/cloud-game/assets/cache --name cloud-game docker.pkg.github.com/giongto35/cloud-game/cloud-game bash -c \"$launchcommand\"'"
     #docker run --privileged -d --network=host -v /cloud-game/games:/cloud-game/assets/games -v /cloud-game/cache:/cloud-game/assets/cache -v /cloud-game/conf.d:/etc/supervisor/conf.d --name cloud-game -e zone=\$zone giongto35/cloud-game-prod supervisord > /tmp/startup.log'"
 
