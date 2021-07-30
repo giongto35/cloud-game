afa3e6837ec00b9277417b9627d5703e350467a9
Sergey Stepanov <sergystepanov@gmail.com>
2021-07-30 23:54:47 +0300

    Extract init function in worker

--- pkg/worker/worker.go
+++ pkg/worker/worker.go
@@ -3,11 +3,14 @@ package worker
 import (
 	"context"
 	"log"
+	"net/http"
 	"time"
 
 	"github.com/giongto35/cloud-game/v2/pkg/config/worker"
 	"github.com/giongto35/cloud-game/v2/pkg/lock"
 	"github.com/giongto35/cloud-game/v2/pkg/monitoring"
+	"github.com/giongto35/cloud-game/v2/pkg/network"
+	"github.com/giongto35/cloud-game/v2/pkg/network/httpx"
 	"github.com/giongto35/cloud-game/v2/pkg/server"
 )
 
@@ -44,9 +47,32 @@ func (wrk *Worker) Run() {
 			h.RequestConfig()
 		}
 		h.Prepare()
-		wrk.spawnServer(wrk.conf.Worker.Server.Address)
+		wrk.init()
 	}()
 	wrk.services.Start()
+}
+
+func (wrk *Worker) init() {
+	conf := wrk.conf.Worker
+
+	address := network.Address(conf.Server.Address)
+	if conf.Server.Https {
+		address = network.Address(conf.Server.Tls.Address)
+	}
+
+	httpx.NewServer(
+		string(address),
+		func(serv *httpx.Server) http.Handler {
+			h := http.NewServeMux()
+			h.HandleFunc("/echo", func(w http.ResponseWriter, r *http.Request) {
+				w.Header().Set("Access-Control-Allow-Origin", "*")
+				log.Println(w, "echo")
+			})
+			return h
+		},
+		httpx.WithServerConfig(conf.Server),
+		httpx.WithPortRoll(true),
+	).Start()
 }
 
 func (wrk *Worker) Shutdown() { wrk.services.Shutdown(wrk.ctx) }
