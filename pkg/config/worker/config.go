16d20cb447f628dc8448099a088dd5c49928551d
Sergey Stepanov <sergystepanov@gmail.com>
2021-08-04 16:49:29 +0300

    Rollback worker ping address builder

--- pkg/config/worker/config.go
+++ pkg/config/worker/config.go
@@ -95,17 +95,17 @@ func (c *Config) expandSpecialTags() {
 // GetPingAddr returns the server for latency check of a zone.
 func (c *Config) GetPingAddr(address string) string {
 	scheme := "http"
-	//host := c.Worker.Server.Address
+	host := c.Worker.Server.Address
 	if c.Worker.Server.Https {
 		scheme = "https"
-	//	host = c.Worker.Server.Tls.Address
+		host = c.Worker.Server.Tls.Address
 	}
-	//if strings.HasPrefix(host, ":") {
-	//	host = "localhost" + host
-	//}
-	//if c.Worker.Network.Zone != "" {
-	//	host = c.Worker.Network.Zone + "." + host
-	//}
+	if strings.HasPrefix(host, ":") {
+		host = "localhost" + host
+	}
+	if c.Worker.Network.Zone != "" {
+		host = c.Worker.Network.Zone + "." + host
+	}
 	u := url.URL{Scheme: scheme, Host: address, Path: c.Worker.Network.PingEndpoint}
 	return u.String()
 }
