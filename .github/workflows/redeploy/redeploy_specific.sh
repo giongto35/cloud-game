3b662e5cad6262763e96241f91d2670db961f341
Sergey Stepanov <sergystepanov@gmail.com>
2021-08-10 20:52:40 +0300

    Use .env in the prod deployment script

--- .github/workflows/redeploy/redeploy_specific.sh
+++ .github/workflows/redeploy/redeploy_specific.sh
@@ -8,23 +8,33 @@ do
     ssh-keyscan -H $ip_address >> ~/.ssh/known_hosts
     sleep 2
 
-    if [ "$ip_address" == "167.172.70.98" ] || [ "$ip_address" == "cloudretro.io" ] 
+    if [ "$ip_address" == "167.172.70.98" ] || [ "$ip_address" == "cloudretro.io" ]
     then
         launchcommand="coordinator > /tmp/startup.log"
     else
         launchcommand="Xvfb :99 & worker --coordinatorhost cloudretro.io --zone \$zone > /tmp/startup.log"
     fi
 
-    ssh root@$ip_address "mkdir -p /cloud-game/configs"
-    rsync ./.github/workflows/redeploy/config.yaml root@$ip_address:/cloud-game/configs/config.yaml
+    ssh root@$ip_address "mkdir -p /cloud-game"
+    rsync ./.github/workflows/redeploy/.env root@$ip_address:/cloud-game/.env
     run_content="'#! /bin/bash
-    echo $PASSWORD | docker login https://docker.pkg.github.com --username $USERNAME --password-stdin; 
+    echo $PASSWORD | docker login https://docker.pkg.github.com --username $USERNAME --password-stdin;
     ufw disable;
     docker system prune -f;
-    source /etc/profile; 
+    source /etc/profile;
     docker pull docker.pkg.github.com/giongto35/cloud-game/cloud-game:latest;
     docker rm cloud-game -f;
-    docker run --privileged -d --network=host --env DISPLAY=:99 --env MESA_GL_VERSION_OVERRIDE=3.3 -v cores:/usr/local/share/cloud-game/assets/cores -v /cloud-game/configs:/usr/local/share/cloud-game/configs -v /cloud-game/games:/usr/local/share/cloud-game/assets/games -v /cloud-game/cache:/usr/local/share/cloud-game/assets/cache --name cloud-game docker.pkg.github.com/giongto35/cloud-game/cloud-game bash -c \"$launchcommand\"'"
+    docker run --privileged -d \
+      --network=host \
+      --env DISPLAY=:99 \
+      --env MESA_GL_VERSION_OVERRIDE=3.3 \
+      --env-file .env \
+      -v cores:/usr/local/share/cloud-game/assets/cores \
+      -v /cloud-game/games:/usr/local/share/cloud-game/assets/games \
+      -v /cloud-game/cache:/usr/local/share/cloud-game/assets/cache \
+      --name cloud-game \
+      docker.pkg.github.com/giongto35/cloud-game/cloud-game \
+      bash -c \"$launchcommand\"'"
 
     ssh root@$ip_address "echo $run_content > ~/run.sh"
     ssh root@$ip_address "chmod +x run.sh; ./run.sh"
