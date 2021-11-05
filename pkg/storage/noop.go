a117be7e80d616b473a533aacf7b7d41134579f1
Sergey Stepanov <sergystepanov@gmail.com>
2021-11-06 00:52:27 +0300

    Update cloud-storage logging

--- pkg/storage/noop.go
+++ pkg/storage/noop.go
@@ -6,14 +6,8 @@ type NoopCloudStorage struct{}
 
 var noopErr = errors.New("an empty storage stub")
 
-func NewNoopCloudStorage() (*NoopCloudStorage, error) {
-	return nil, noopErr
-}
+func NewNoopCloudStorage() (*NoopCloudStorage, error) { return nil, noopErr }
 
-func (n *NoopCloudStorage) Save(_ string, _ string) (err error) {
-	return nil
-}
+func (n *NoopCloudStorage) Save(_ string, _ string) (err error) { return nil }
 
-func (n *NoopCloudStorage) Load(_ string) (data []byte, err error) {
-	return nil, noopErr
-}
+func (n *NoopCloudStorage) Load(_ string) (data []byte, err error) { return nil, noopErr }
