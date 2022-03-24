7ab2645d99cd327a141d593e9d817d5a330e175d
Sergey Stepanov <sergystepanov@gmail.com>
2022-03-24 19:40:31 +0300

    Move server list handlers outside the main function

--- web/js/serverList.js
+++ web/js/serverList.js
@@ -6,26 +6,26 @@ const serverList = (() => {
     const id = 'server-list',
         root = document.getElementById(id),
         index = ((i = 1) => () => i++)(),
-        // cap -- is a caption of the field
-        // mut -- is an arbitrary mutation of the field
+        // caption -- the field caption
+        // renderer -- is an arbitrary mutation of the field
         list = {
             'n': {
-                // print line number as 01
-                mut: () => String(index()).padStart(2, '0')
+                renderer: () => String(index()).padStart(2, '0')
             },
             'id': {
-                cap: 'ID', mut: (data) => data?.id ? data.id : `??? [replicated] x ${data['replicas']}`
+                caption: 'ID',
+                renderer: (data) => data?.id ? data.id : `??? [replicated] x ${data['replicas']}`
             },
             'addr': {
-                cap: 'Address', mut: (data) => data?.port ? `${data.addr}:${data.port}` : data.addr
+                caption: 'Address',
+                renderer: (data) => data?.port ? `${data.addr}:${data.port}` : data.addr
             },
-            'is_busy': {cap: 'State', mut: (data) => data.is_busy === true ? 'X' : ''},
+            'is_busy': {
+                caption: 'State',
+                renderer: (data) => data.is_busy === true ? 'X' : ''
+            },
             'use': {
-                mut: (_, handler) => gui.create('a', (el) => {
-                    el.innerText = '>>';
-                    el.href = "#";
-                    el.addEventListener('click', handler);
-                })
+                renderer: renderServerChangeEl
             }
         },
         fields = Object.keys(list);
@@ -33,19 +33,15 @@ const serverList = (() => {
     // root.classList.add("hidden");
 
     const state = {
-        servers: [],
         shown: true,
     }
 
-    // waiting for the server connection when it's ready
+    // waiting for the socket to request data
     const onReady = () => socket.getServerList()
 
-    const handleGetServerList = (data) => {
-        state.servers = data?.servers ? data.servers : [];
-        _render();
-    }
+    const onNewData = (dat = {servers: []}) => _render(dat?.servers)
 
-    function _render() {
+    function _render(servers = []) {
         if (!state.shown) {
             gui.hide(root);
             return;
@@ -53,7 +49,7 @@ const serverList = (() => {
         root.innerHTML = '';
         gui.show(root);
 
-        if (state.servers.length === 0) {
+        if (servers.length === 0) {
             root.append(gui.create('span', (el) => el.innerText = 'No data :('));
             return;
         }
@@ -61,33 +57,33 @@ const serverList = (() => {
         const frag = gui.fragment();
         const header = gui.create('div', (el) => {
             el.classList.add(`${id}__header`);
-            fields.forEach(field => el.append(gui.create('span', (f) => f.innerHTML = list[field]?.cap || '')))
+            fields.forEach(field => el.append(gui.create('span', (f) => f.innerHTML = list[field]?.caption || '')))
         });
-        frag.appendChild(header)
+        frag.append(header)
 
         const renderRow = (server) => (row) => fields.forEach(field => {
             const val = server.hasOwnProperty(field) ? server[field] : '';
-            const mut = list[field]?.mut;
-            row.appendChild(gui.create('span', (f) => {
-                    if (!mut) {
-                        f.innerHTML = val;
-                        return;
-                    }
-                    const content = mut(server, () => console.log(server.addr, server.id));
-                    if (content.nodeType) {
-                        f.appendChild(content);
-                        return;
-                    }
-                    f.innerHTML = content
-                })
-            );
+            const renderer = list[field]?.renderer;
+            row.append(gui.create('span', (f) => f.append(renderer ? renderer(server) : val)));
         })
-        state.servers.forEach(server => frag.appendChild(gui.create('div', renderRow(server))))
-        root.appendChild(frag);
+        servers.forEach(server => frag.append(gui.create('div', renderRow(server))))
+        root.append(frag);
+    }
+
+    function renderServerChangeEl(server) {
+        const handleServerChange = (e) => {
+            e.preventDefault();
+            console.log(server.addr, server.id);
+        }
+        return gui.create('a', (el) => {
+            el.innerText = '>>';
+            el.href = "#";
+            el.addEventListener('click', handleServerChange);
+        })
     }
 
     event.sub(SOCKET_READY, onReady);
-    event.sub(GET_SERVER_LIST, handleGetServerList);
+    event.sub(GET_SERVER_LIST, onNewData);
 
     return {}
 })(document, event, gui, log, socket);
