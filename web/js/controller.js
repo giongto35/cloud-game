be0e00c7bf812d31b8033f177341d25f3dd159f5
Sergey Stepanov <sergystepanov@gmail.com>
2022-04-18 22:47:35 +0300

    Use display: none in the hide function, so it won't take any space

--- web/js/controller.js
+++ web/js/controller.js
@@ -98,7 +98,6 @@
             gui.toggle(keyButtons[KEY.LOAD], show || isGameScreen);
 
             gui.toggle(helpOverlay, show)
-            Array.from(helpOverlay.children).forEach((node) => gui.toggle(node, show))
 
             this.shown = show;
 
