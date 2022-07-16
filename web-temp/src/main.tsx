3b4a8b141e72e27d2d65122cc770a2f71bd2f43b
eraser5th <83107074+eraser5th@users.noreply.github.com>
2022-07-16 17:26:39 +0900

    use the-new-css-reset

--- web-temp/src/main.tsx
+++ web-temp/src/main.tsx
@@ -1,10 +1,11 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "the-new-css-reset/css/reset.css";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
-)
+);
