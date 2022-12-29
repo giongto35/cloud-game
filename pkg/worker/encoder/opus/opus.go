8b19dff5a93e419cd23243c1a3c829697caa0dbe
Sergey Stepanov <sergystepanov@gmail.com>
2022-12-29 03:35:11 +0300

    Update Opus

--- pkg/worker/encoder/opus/opus.go
+++ pkg/worker/encoder/opus/opus.go
@@ -40,131 +40,152 @@ const (
 	// FullBand is 20 kHz bandpass
 	FullBand = Bandwidth(C.OPUS_BANDWIDTH_FULLBAND)
 )
+const stereo = C.int(2)
 
-type Opus struct {
+type Encoder struct {
 	mem []byte
+	out []byte
 	st  *C.struct_OpusEncoder
 }
 
-// NewOpusEncoder creates new Opus encoder.
-func NewOpusEncoder(sampleRate int, app Application) (*Opus, error) {
-	var enc Opus
-	if enc.st != nil {
-		return nil, fmt.Errorf("opus: encoder reinit")
+func NewEncoder(outFq int) (*Encoder, error) {
+	mem := make([]byte, C.opus_encoder_get_size(stereo))
+	out := make([]byte, 1024)
+	enc := Encoder{
+		mem: mem,
+		st:  (*C.OpusEncoder)(unsafe.Pointer(&mem[0])),
+		out: out,
 	}
-	const stereo = C.int(2)
-	enc.mem = make([]byte, C.opus_encoder_get_size(stereo))
-	enc.st = (*C.OpusEncoder)(unsafe.Pointer(&enc.mem[0]))
-	err := unwrap(C.opus_encoder_init(enc.st, C.opus_int32(sampleRate), stereo, C.int(app)))
+	err := unwrap(C.opus_encoder_init(enc.st, C.opus_int32(outFq), stereo, C.int(AppRestrictedLowDelay)))
 	if err != nil {
 		return nil, fmt.Errorf("opus: initializatoin error (%v)", err)
 	}
+	_ = enc.SetMaxBandwidth(FullBand)
+	_ = enc.SetBitrate(96000)
+	_ = enc.SetComplexity(5)
+
 	return &enc, nil
 }
 
-// Encode converts raw PCM samples into the supplied Opus buffer.
-// Returns the number of bytes converted.
-func (enc *Opus) Encode(pcm []int16, data []byte) (rez int, err error) {
+func (e *Encoder) Reset() error { return e.ResetState() }
+
+func (e *Encoder) Encode(pcm []int16) ([]byte, error) {
 	if len(pcm) == 0 {
-		return
+		return nil, nil
 	}
-	n := C.opus_encode(enc.st, (*C.opus_int16)(&pcm[0]), C.int(len(pcm)>>1), (*C.uchar)(&data[0]), C.opus_int32(cap(data)))
-	if n > 0 {
-		rez = int(n)
+	n := C.opus_encode(e.st, (*C.opus_int16)(&pcm[0]), C.int(len(pcm)>>1), (*C.uchar)(&e.out[0]), C.opus_int32(cap(pcm)))
+	err := unwrap(n)
+	// n = 1 is DTX
+	if err != nil || n == 1 {
+		return []byte{}, err
 	}
-	return rez, unwrap(n)
+	return e.out[:int(n)], nil
+}
+
+func (e *Encoder) GetInfo() string {
+	bitrate, _ := e.Bitrate()
+	complexity, _ := e.Complexity()
+	dtx, _ := e.DTX()
+	fec, _ := e.FEC()
+	maxBandwidth, _ := e.MaxBandwidth()
+	lossPercent, _ := e.PacketLossPerc()
+	sampleRate, _ := e.SampleRate()
+	return fmt.Sprintf(
+		"%v, Bitrate: %v bps, Complexity: %v, DTX: %v, FEC: %v, Max bandwidth: *%v, Loss%%: %v, Rate: %v Hz",
+		CodecVersion(), bitrate, complexity, dtx, fec, maxBandwidth, lossPercent, sampleRate,
+	)
 }
 
 // SampleRate returns the sample rate of the encoder.
-func (enc *Opus) SampleRate() (int, error) {
+func (e *Encoder) SampleRate() (int, error) {
 	var sampleRate C.opus_int32
-	res := C.get_sample_rate(enc.st, &sampleRate)
+	res := C.get_sample_rate(e.st, &sampleRate)
 	return int(sampleRate), unwrap(res)
 }
 
 // Bitrate returns the bitrate of the encoder.
-func (enc *Opus) Bitrate() (int, error) {
+func (e *Encoder) Bitrate() (int, error) {
 	var bitrate C.opus_int32
-	res := C.get_bitrate(enc.st, &bitrate)
+	res := C.get_bitrate(e.st, &bitrate)
 	return int(bitrate), unwrap(res)
 }
 
 // SetBitrate sets the bitrate of the encoder.
 // BitrateMax / BitrateAuto can be used here.
-func (enc *Opus) SetBitrate(b Bitrate) error {
-	return unwrap(C.set_bitrate(enc.st, C.opus_int32(b)))
+func (e *Encoder) SetBitrate(b Bitrate) error {
+	return unwrap(C.set_bitrate(e.st, C.opus_int32(b)))
 }
 
 // Complexity returns the value of the complexity.
-func (enc *Opus) Complexity() (int, error) {
+func (e *Encoder) Complexity() (int, error) {
 	var complexity C.opus_int32
-	res := C.get_complexity(enc.st, &complexity)
+	res := C.get_complexity(e.st, &complexity)
 	return int(complexity), unwrap(res)
 }
 
 // SetComplexity sets the complexity factor for the encoder.
 // Complexity is a value from 1 to 10, where 1 is the lowest complexity and 10 is the highest.
-func (enc *Opus) SetComplexity(complexity int) error {
-	return unwrap(C.set_complexity(enc.st, C.opus_int32(complexity)))
+func (e *Encoder) SetComplexity(complexity int) error {
+	return unwrap(C.set_complexity(e.st, C.opus_int32(complexity)))
 }
 
 // DTX says if discontinuous transmission is enabled.
-func (enc *Opus) DTX() (bool, error) {
+func (e *Encoder) DTX() (bool, error) {
 	var dtx C.opus_int32
-	res := C.get_dtx(enc.st, &dtx)
+	res := C.get_dtx(e.st, &dtx)
 	return dtx > 0, unwrap(res)
 }
 
 // SetDTX switches discontinuous transmission.
-func (enc *Opus) SetDTX(dtx bool) error {
+func (e *Encoder) SetDTX(dtx bool) error {
 	var i int
 	if dtx {
 		i = 1
 	}
-	return unwrap(C.set_dtx(enc.st, C.opus_int32(i)))
+	return unwrap(C.set_dtx(e.st, C.opus_int32(i)))
 }
 
 // MaxBandwidth returns the maximum allowed bandpass value.
-func (enc *Opus) MaxBandwidth() (Bandwidth, error) {
+func (e *Encoder) MaxBandwidth() (Bandwidth, error) {
 	var b C.opus_int32
-	res := C.get_max_bandwidth(enc.st, &b)
+	res := C.get_max_bandwidth(e.st, &b)
 	return Bandwidth(b), unwrap(res)
 }
 
 // SetMaxBandwidth sets the upper limit of the bandpass.
-func (enc *Opus) SetMaxBandwidth(b Bandwidth) error {
-	return unwrap(C.set_max_bandwidth(enc.st, C.opus_int32(b)))
+func (e *Encoder) SetMaxBandwidth(b Bandwidth) error {
+	return unwrap(C.set_max_bandwidth(e.st, C.opus_int32(b)))
 }
 
 // FEC says if forward error correction (FEC) is enabled.
-func (enc *Opus) FEC() (bool, error) {
+func (e *Encoder) FEC() (bool, error) {
 	var fec C.opus_int32
-	res := C.get_inband_fec(enc.st, &fec)
+	res := C.get_inband_fec(e.st, &fec)
 	return fec > 0, unwrap(res)
 }
 
 // SetFEC switches the forward error correction (FEC).
-func (enc *Opus) SetFEC(fec bool) error {
+func (e *Encoder) SetFEC(fec bool) error {
 	var i int
 	if fec {
 		i = 1
 	}
-	return unwrap(C.set_inband_fec(enc.st, C.opus_int32(i)))
+	return unwrap(C.set_inband_fec(e.st, C.opus_int32(i)))
 }
 
 // PacketLossPerc returns configured packet loss percentage.
-func (enc *Opus) PacketLossPerc() (int, error) {
+func (e *Encoder) PacketLossPerc() (int, error) {
 	var lossPerc C.opus_int32
-	res := C.get_packet_loss_perc(enc.st, &lossPerc)
+	res := C.get_packet_loss_perc(e.st, &lossPerc)
 	return int(lossPerc), unwrap(res)
 }
 
 // SetPacketLossPerc sets expected packet loss percentage.
-func (enc *Opus) SetPacketLossPerc(lossPerc int) error {
-	return unwrap(C.set_packet_loss_perc(enc.st, C.opus_int32(lossPerc)))
+func (e *Encoder) SetPacketLossPerc(lossPerc int) error {
+	return unwrap(C.set_packet_loss_perc(e.st, C.opus_int32(lossPerc)))
 }
 
-func (enc *Opus) ResetState() error { return unwrap(C.reset_state(enc.st)) }
+func (e *Encoder) ResetState() error { return unwrap(C.reset_state(e.st)) }
 
 func (e Error) Error() string { return fmt.Sprintf("opus: %v", C.GoString(C.opus_strerror(C.int(e)))) }
 
