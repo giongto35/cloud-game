e30371b55fc22e29078429850e25999840fb4c66
Sergey Stepanov <sergystepanov@gmail.com>
2023-02-28 23:36:32 +0300

    Add some API description

--- pkg/api/api.go
+++ pkg/api/api.go
@@ -1,9 +1,24 @@
+// Package api defines the general API for both coordinator and worker applications.
+//
+// Each API call (request and response) is a JSON-encoded "packet" of the following structure:
+//
+//	id - (optional) a globally unique packet id;
+//	 t - (required) one of the predefined unique packet types;
+//	 p - (optional) packet payload with arbitrary data.
+//
+// The basic idea behind this API is that the packets differentiate by their predefined types
+// with which it is possible to unwrap the payload into distinct request/response data structures.
+// And the id field is used for tracking packets through a chain of different network points (apps, devices),
+// for example, passing a packet from a browser forward to a worker and back through a coordinator.
+//
+// Example:
+//
+//	{"t":4,"p":{"ice":[{"urls":"stun:stun.l.google.com:19302"}],"games":["Sushi The Cat"],"wid":"cfv68irdrc3ifu3jn6bg"}}
 package api
 
 import (
+	"encoding/json"
 	"fmt"
-
-	"github.com/goccy/go-json"
 )
 
 type (
@@ -27,7 +42,7 @@ type (
 type In[I Id] struct {
 	Id      I               `json:"id,omitempty"`
 	T       PT              `json:"t"`
-	Payload json.RawMessage `json:"p,omitempty"`
+	Payload json.RawMessage `json:"p,omitempty"` // should be json.RawMessage for 2-pass unmarshal
 }
 
 func (i In[I]) GetId() I           { return i.Id }
