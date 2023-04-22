61a16e5a46fe8dd12a62f20a69df11a222b60546
Sergey Stepanov <sergystepanov@gmail.com>
2023-04-22 15:11:20 +0300

    Remove hackish way of adding ICE servers
    Use config files instead (k8s ConfigMap or something).

--- pkg/config/webrtc/config.go
+++ pkg/config/webrtc/config.go
@@ -1,12 +1,5 @@
 package webrtc
 
-import (
-	"log"
-	"strings"
-
-	"github.com/giongto35/cloud-game/v3/pkg/config"
-)
-
 type Webrtc struct {
 	DisableDefaultInterceptors bool
 	DtlsRole                   byte
@@ -31,23 +24,3 @@ func (w *Webrtc) HasDtlsRole() bool   { return w.DtlsRole > 0 }
 func (w *Webrtc) HasPortRange() bool  { return w.IcePorts.Min > 0 && w.IcePorts.Max > 0 }
 func (w *Webrtc) HasSinglePort() bool { return w.SinglePort > 0 }
 func (w *Webrtc) HasIceIpMap() bool   { return w.IceIpMap != "" }
-
-func (w *Webrtc) AddIceServersEnv() {
-	cfg := Webrtc{IceServers: []IceServer{{}, {}, {}, {}, {}}}
-	_ = config.LoadConfigEnv(&cfg)
-	for i, ice := range cfg.IceServers {
-		if ice.Urls == "" {
-			continue
-		}
-		if strings.HasPrefix(ice.Urls, "turn:") || strings.HasPrefix(ice.Urls, "turns:") {
-			if ice.Username == "" || ice.Credential == "" {
-				log.Fatalf("TURN or TURNS servers should have both username and credential: %+v", ice)
-			}
-		}
-		if i > len(w.IceServers)-1 {
-			w.IceServers = append(w.IceServers, ice)
-		} else {
-			w.IceServers[i] = ice
-		}
-	}
-}
