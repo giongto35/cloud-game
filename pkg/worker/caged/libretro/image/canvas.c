e23e2843c5de8eb38667b24f2540668abaa01220
Sergey Stepanov <sergystepanov@gmail.com>
2023-09-24 01:55:18 +0300

    Clean canvas.c

--- pkg/worker/caged/libretro/image/canvas.c
+++ pkg/worker/caged/libretro/image/canvas.c
@@ -3,21 +3,25 @@
 __inline xy rotate(int t, int x, int y, int w, int h) {
     xy p = {x, y};
     switch (t) {
+        // 90° CCW or 270° CW
         case A90:
-            p.x = r90_x(x,y,w,h);
-            p.y = r90_y(x,y,w,h);
+            p.x = y;
+            p.y = w - 1 - x;
             break;
+        // 180° CCW
         case A180:
-            p.x = r180_x(x,y,w,h);
-            p.y = r180_y(x,y,w,h);
+            p.x = w - 1 - x;
+            p.y = h - 1 - y;
             break;
+        // 270° CCW or 90° CW
         case A270:
-            p.x = r270_x(x,y,w,h);
-            p.y = r270_y(x,y,w,h);
+            p.x = h - 1 - y;
+            p.y = x;
             break;
+        // flip Y
         case F180:
-            p.x = fy180_x(x,y,w,h);
-            p.y = fy180_y(x,y,w,h);
+            //p.x = x;
+            p.y = h - 1 - y;
             break;
     }
     return p;
@@ -28,20 +32,20 @@ __inline uint32_t _565(uint32_t x) {
 }
 
 __inline uint32_t _8888rev(uint32_t px) {
-    return (((px >> 16) & 0xff) | (px & 0xff00) | ((px << 16) & 0xff0000)); // | 0xff000000)
+    return (((px >> 16) & 0xff) | (px & 0xff00) | ((px << 16) & 0xff0000)); // | 0xff000000
 }
 
-void RGBA(int pix, uint32_t *__restrict dst, void *__restrict source, int y, int h, int w, int hh, int dw, int pad, int rot) {
+void RGBA(int pix, uint32_t *__restrict dst, const void *__restrict source, int y, int h, int w, int hh, int dw, int pad, int rot) {
     int x;
     xy rxy;
-    uint16_t *src16;
-    uint32_t *src32;
+    const uint16_t *src16;
+    const uint32_t *src32;
 
     switch (pix) {
         //case BIT_SHORT5551:
         //    break;
         case BIT_INT_8888REV:
-            src32 = (uint32_t *)source;
+            src32 = (const uint32_t *)source;
             int pad32 = pad >> 2;
             if (rot == NO_ROT) {
                 for (; y < h; ++y) {
@@ -54,14 +58,14 @@ void RGBA(int pix, uint32_t *__restrict dst, void *__restrict source, int y, int
                 for (; y < h; ++y) {
                     for (x = 0; x < w; ++x) {
                         rxy = rotate(rot, x, y, w, hh);
-                        dst[rxy.x+rxy.y*w] = _8888rev(*src32++);
+                        dst[rxy.x+rxy.y*dw] = _8888rev(*src32++);
                     }
                     src32 += pad32;
                 }
             }
             break;
         case BIT_SHORT565:
-            src16 = (uint16_t *)source;
+            src16 = (const uint16_t *)source;
             int pad16 = pad >> 1;
             if (rot == NO_ROT) {
                 for (; y < h; ++y) {
--- pkg/worker/caged/libretro/image/canvas.h
+++ pkg/worker/caged/libretro/image/canvas.h
@@ -13,29 +13,13 @@
 #define	A270 3
 #define	F180 4
 
-// Rotate90 is 90° CCW or 270° CW.
-#define r90_x(x, y, w, h) ( y )
-#define r90_y(x, y, w, h) ( (w - 1) - x )
-
-// Rotate180 is 180° CCW.
-#define r180_x(x, y, w, h) ( (w - 1) - x )
-#define r180_y(x, y, w, h) ( (h - 1) - y )
-
-// Rotate270 is 270° CCW or 90° CW.
-#define r270_x(x, y, w, h) ( (h - 1) - y )
-#define r270_y(x, y, w, h) ( x )
-
-// Flip Y
-#define fy180_x(x, y, w, h) ( x )
-#define fy180_y(x, y, w, h) ( (h - 1) - y )
-
 typedef struct XY {
     int x, y;
 } xy;
 
 xy rotate(int t, int x, int y, int w, int h);
 
-void RGBA(int pix, uint32_t *dst, void *source, int y, int h, int w, int hh, int dw, int pad, int rot);
+void RGBA(int pix, uint32_t *dst, const void *source, int y, int h, int w, int hh, int dw, int pad, int rot);
 
 uint32_t _565(uint32_t x);
 uint32_t _8888rev(uint32_t px);
