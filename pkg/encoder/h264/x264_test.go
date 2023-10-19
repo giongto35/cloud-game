501071663634a8dc29ee7db3f47f229fea88898c
Sergey Stepanov <sergystepanov@gmail.com>
2023-10-19 17:15:25 +0300

    Don't copy YUV planes in x264

---
+++ pkg/encoder/h264/x264_test.go
@@ -0,0 +1,29 @@
+package h264
+
+import "testing"
+
+func TestH264Encode(t *testing.T) {
+	h264, err := NewEncoder(120, 120, nil)
+	if err != nil {
+		t.Error(err)
+	}
+	data := make([]byte, 120*120*1.5)
+	h264.LoadBuf(data)
+	h264.Encode()
+	if err := h264.Shutdown(); err != nil {
+		t.Error(err)
+	}
+}
+
+func Benchmark(b *testing.B) {
+	w, h := 1920, 1080
+	h264, err := NewEncoder(w, h, nil)
+	if err != nil {
+		b.Error(err)
+	}
+	data := make([]byte, int(float64(w)*float64(h)*1.5))
+	for i := 0; i < b.N; i++ {
+		h264.LoadBuf(data)
+		h264.Encode()
+	}
+}
