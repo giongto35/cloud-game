7c604e94fb18420c2f0f3027a2978fc7e51c1fe9
sergystepanov <sergystepanov@gmail.com>
2026-06-03 18:40:55 +0300

    Reformat socket.js

--- web/js/network/socket.js
+++ web/js/network/socket.js
@@ -1,43 +1,33 @@
-import {
-    pub,
-    MESSAGE
-} from 'event';
-import {log} from 'log';
+import { pub, MESSAGE } from "event";
+import { log } from "log";
 
 let conn;
 
 const buildUrl = (params = {}) => {
     const url = new URL(window.location);
-    url.protocol = location.protocol !== 'https:' ? 'ws' : 'wss';
+    url.protocol = location.protocol !== "https:" ? "ws" : "wss";
     url.pathname = "/ws";
-    Object.keys(params).forEach(k => {
-        if (!!params[k]) url.searchParams.set(k, params[k])
-    })
-    return url
-}
+    Object.keys(params).forEach((k) => {
+        if (params[k]) url.searchParams.set(k, params[k]);
+    });
+    return url;
+};
 
 const init = (roomId, wid, zone) => {
-    let objParams = {room_id: roomId, zone: zone};
+    let objParams = { room_id: roomId, zone: zone };
     if (wid) objParams.wid = wid;
-    const url = buildUrl(objParams)
-    log.info(`[ws] connecting to ${url}`);
+    const url = buildUrl(objParams);
+    log.debug(`[ws] connecting to ${url}`);
     conn = new WebSocket(url.toString());
-    conn.onopen = () => {
-        log.info('[ws] <- open connection');
-    };
-    conn.onerror = () => log.error('[ws] some error!');
-    conn.onclose = (event) => log.info(`[ws] closed (${event.code})`);
-    conn.onmessage = response => {
-        const data = JSON.parse(response.data);
-        pub(MESSAGE, data);
-    };
+    conn.onopen = () => log.debug("[ws] opened");
+    conn.onerror = () => log.error("[ws] error");
+    conn.onclose = (event) => log.debug(`[ws] closed (${event.code})`);
+    conn.onmessage = (response) => pub(MESSAGE, JSON.parse(response.data));
 };
 
 const send = (data) => {
-    if (conn.readyState === 1) {
-        conn.send(JSON.stringify(data));
-    }
-}
+    if (conn.readyState === 1) conn.send(JSON.stringify(data));
+};
 
 /**
  * WebSocket connection module.
@@ -46,5 +36,5 @@ const send = (data) => {
  */
 export const socket = {
     init,
-    send
-}
+    send,
+};
